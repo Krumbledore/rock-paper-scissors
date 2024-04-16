878c0e0ac54dfc236440eb686e00903fe286b3b6
Krumbledore <45559583+Krumbledore@users.noreply.github.com>
2024-04-15 19:03:33 -0800

    Add win, lose and draw functions

--- script.js
+++ script.js
@@ -18,16 +18,18 @@ function playRound(playerSelection, computerSelection){
         console.log("ROCK, PAPER, SCISSORS!\n DRAW: " + playerSelection + " vs " + computerSelection);
 
         console.log("Your score: " + playerScore + " | Computer score: " + computerScore);
+        draw(playerSelection, computerSelection);
         //User plays Rock    
     }else if( playerSelection === "rock" && computerSelection === "paper"){
         console.log("ROCK, PAPER, SCISSORS!\n YOU LOSE!: " + playerSelection + " vs " + computerSelection);
         computerScore = computerScore + 1;
         console.log("Your score: " + playerScore + " | Computer score: " + computerScore);
-    
+        lose(playerSelection, computerSelection);
     }else if( playerSelection === "rock" && computerSelection === "scissors"){
         console.log("ROCK, PAPER, SCISSORS!\n YOU WIN!: " + playerSelection + " vs " + computerSelection);
         playerScore = playerScore + 1;
         console.log("Your score: " + playerScore + " | Computer score: " + computerScore);
+        win(playerSelection, computerSelection);
     
 
         //User plays Paper
@@ -35,24 +37,26 @@ function playRound(playerSelection, computerSelection){
         console.log("ROCK, PAPER, SCISSORS!\n YOU LOSE!: " + playerSelection + " vs " + computerSelection);
         computerScore = computerScore + 1;
         console.log("Your score: " + playerScore + " | Computer score: " + computerScore);
-    
+        lose(playerSelection, computerSelection);
+   
     }else if( playerSelection === "paper" && computerSelection === "rock"){
         console.log("ROCK, PAPER, SCISSORS!\n YOU WIN!: " + playerSelection + " vs " + computerSelection);
         playerScore = playerScore + 1;
         console.log("Your score: " + playerScore + " | Computer score: " + computerScore);
-    
+        win(playerSelection, computerSelection);
     
         //User plays Scissors
     }else if( playerSelection === "scissors" && computerSelection === "rock"){
         console.log("ROCK, PAPER, SCISSORS!\n YOU LOSE!: " + playerSelection + " vs " + computerSelection);
         computerScore = computerScore + 1;
         console.log("Your score: " + playerScore + " | Computer score: " + computerScore);
+        lose(playerSelection, computerSelection)
     
     }else if( playerSelection === "scissors" && computerSelection === "paper"){
         console.log("ROCK, PAPER, SCISSORS!\n YOU WIN!: " + playerSelection + " vs " + computerSelection);
         playerScore = playerScore + 1;
         console.log("Your score: " + playerScore + " | Computer score: " + computerScore);
-    
+        win(playerSelection, computerSelection);
     
         
     }else if(!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors' )){
@@ -81,8 +85,32 @@ menu.addEventListener('click', (e) =>{
             break;
     }
 })
+let resultText = document.createElement('p')
+let info = document.querySelector('.game-info');
 
+function win(playerSelection, computerSelection){
+    let p = document.createElement('p')
+    resultText.textContent = "YOU WIN!: " + playerSelection + " vs " + computerSelection;
+    resultText.style.color = 'green';
+    
+    info.appendChild(resultText)
+}
 
+function lose(playerSelection, computerSelection){
+    let p = document.createElement('p')
+    resultText.textContent = "YOU LOSE!: " + playerSelection + " vs " + computerSelection;
+    resultText.style.color = 'red';
+    
+    info.appendChild(resultText)
+}
+
+function draw(playerSelection, computerSelection){
+    let p = document.createElement('p')
+    resultText.textContent = "DRAW!: " + playerSelection + " vs " + computerSelection;
+    resultText.style.color = 'yellow';
+    
+    info.appendChild(resultText)
+}
 
 let playerSelection = '';
 const computerSelection = getComputerChoice();
